17083fe31bed131efa45573d4bcd6a25c65d7864
Biraj Rai <valxud1@gmail.com>
2023-02-19 17:37:42 +0545

    e

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log('I Lost Everything!');
+const toggleNav = document.querySelector('.toggle-nav');
+const nav = document.querySelector('nav ul');
+
+toggleNav.addEventListener('click', function () {
+    nav.classList.toggle('show');
+});
