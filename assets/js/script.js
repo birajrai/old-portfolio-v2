61a1bb7890cba4349207eb18774b5661ddc087b9
Biraj Rai <valxud1@gmail.com>
2023-03-02 21:51:24 +0545

    feat: tailwindcss cdn in prod :')

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
 console.log('I Lost Everything!');
-const toggleNav = document.querySelector('.toggle-nav');
-const nav = document.querySelector('nav ul');
-
-toggleNav.addEventListener('click', function () {
-    nav.classList.toggle('show');
-});
